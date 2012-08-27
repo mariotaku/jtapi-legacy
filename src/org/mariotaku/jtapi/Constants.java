7f46b61f4ef11092e3a7aaf245d5114b69462849
Mariotaku Lee <mariotaku.lee@gmail.com>
2012-08-27 18:45:13 +0800

    0.2 version.
    image upload supported.
    please try it on non-GAE services, I removed GAE dependicies.

--- src/org/mariotaku/jtapi/Constants.java
+++ src/org/mariotaku/jtapi/Constants.java
@@ -4,8 +4,8 @@ import java.util.regex.Pattern;
 
 public interface Constants {
 
-	public static final String VERSION_NAME = "0.1";
-	
+	public static final String VERSION_NAME = "0.2";
+
 	public static final Pattern PATTERN_SUB_DOMAIN = Pattern.compile("([\\d\\w\\-]+)\\.([\\d\\w\\-]+)\\.appspot\\.com");
 	public static final int ID_SUB_DOMAIN = 1;
 }
--- src/org/mariotaku/jtapi/JtapiServlet.java
+++ src/org/mariotaku/jtapi/JtapiServlet.java
@@ -1,23 +1,21 @@
 package org.mariotaku.jtapi;
 
-import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.Enumeration;
 import java.util.List;
+import java.util.Map;
 import java.util.regex.Matcher;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-import com.google.appengine.api.urlfetch.HTTPHeader;
-import com.google.appengine.api.urlfetch.HTTPMethod;
-import com.google.appengine.api.urlfetch.HTTPRequest;
-import com.google.appengine.api.urlfetch.HTTPResponse;
-import com.google.appengine.api.urlfetch.URLFetchService;
-import com.google.appengine.api.urlfetch.URLFetchServiceFactory;
 
 @SuppressWarnings("serial")
 public class JtapiServlet extends HttpServlet implements Constants {
@@ -47,6 +45,16 @@ public class JtapiServlet extends HttpServlet implements Constants {
 		handleRequest(req, resp);
 	}
 
+	private void copyStream(InputStream is, OutputStream os) throws IOException {
+		final int buffer_size = 1024;
+		final byte[] bytes = new byte[buffer_size];
+		int count = is.read(bytes, 0, buffer_size);
+		while (count != -1) {
+			os.write(bytes, 0, count);
+			count = is.read(bytes, 0, buffer_size);
+		}
+	}
+
 	private void handleRequest(HttpServletRequest req, HttpServletResponse resp) throws IOException {
 		final String server_name = req.getServerName();
 		final String request_uri = req.getRequestURI();
@@ -55,54 +63,69 @@ public class JtapiServlet extends HttpServlet implements Constants {
 		if (sub_domain != null) {
 			final String twitter_domain = sub_domain + ".twitter.com";
 			final String query_param = req.getQueryString();
-			final String request_url = "https://" + twitter_domain + request_uri + (query_param != null ? "?" + query_param : "");
-			final URLFetchService service = URLFetchServiceFactory.getURLFetchService();
-			final BufferedReader reader = req.getReader();
-			final StringBuilder builder = new StringBuilder();
-			int c;
-			while ((c = reader.read()) != -1) {
-				if (c != -1) {
-					builder.append((char)c);
-				}
-			}
-			final HTTPRequest request = new HTTPRequest(new URL(request_url), HTTPMethod.valueOf(req.getMethod()));
-			request.setPayload(builder.toString().getBytes());
+			final String request_url_string = "https://" + twitter_domain + request_uri
+					+ (query_param != null ? "?" + query_param : "");
+			final URL request_url = new URL(request_url_string);
+			final String request_method = req.getMethod();
+			final HttpURLConnection conn = (HttpURLConnection) request_url.openConnection();
+			conn.setDoInput(true);
+			conn.setDoOutput(true);
+			conn.setRequestMethod(request_method);
+			conn.setInstanceFollowRedirects(false);
 			final Enumeration<?> header_names = req.getHeaderNames();
 			while (header_names.hasMoreElements()) {
-				final String headerName = (String) header_names.nextElement();
-				final HTTPHeader header = new HTTPHeader(headerName, req.getHeader(headerName));
-				request.addHeader(header);
+				final String header_name = (String) header_names.nextElement();
+				conn.addRequestProperty(header_name, req.getHeader(header_name));
 			}
-			final HTTPResponse api_response = service.fetch(request);
-			final List<HTTPHeader> api_headers = api_response.getHeadersUncombined();
-			for (final HTTPHeader api_header : api_headers) {
-				resp.addHeader(api_header.getName(), api_header.getValue());
+			if ("POST".equals(request_method) || "PUT".equals(request_method)) {
+				copyStream(req.getInputStream(), conn.getOutputStream());
 			}
-			resp.setStatus(api_response.getResponseCode());
-			final byte[] content = api_response.getContent();
-			
+			resp.setStatus(conn.getResponseCode());
+			resp.setContentType(conn.getContentType());
+			final Map<String, List<String>> api_headers = conn.getHeaderFields();
+			for (final String key : api_headers.keySet()) {
+				for (final String value : api_headers.get(key)) {
+					resp.addHeader(key, value);
+				}
+			}
+			final ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+			copyStream(conn.getInputStream(), buffer);
+			buffer.flush();
+			final byte[] content = buffer.toByteArray();
+
 			if ("api".equals(sub_domain) && request_uri.startsWith("/oauth/authorize")) {
-				final String content_string = new String(content, "UTF-8");
-				final String replaced_content_string = content_string.replace("https://api.twitter.com/oauth/authorize", "https://" + server_name + request_uri);
-				final byte[] replaced_content = replaced_content_string.getBytes("UTF-8");
-				resp.getOutputStream().write(replaced_content);
+				resp.getOutputStream().write(modifyAuthorizePage(req, content));
 			} else {
-				resp.getOutputStream().write(api_response.getContent());
+				resp.getOutputStream().write(content);
 			}
 		} else {
-			resp.setContentType("text/plain");
-			final String scheme = req.getScheme();
-			final PrintWriter writer = resp.getWriter();
-			writer.println("JTAPI " + VERSION_NAME + " is running!");
-			writer.println("--------------------------------");
-			writer.println("Rest Base URL:		" + scheme + "://" + "api." + server_name + "/1/");
-			writer.println("Oauth Base URL: 	" + scheme + "://" + "api." + server_name + "/oauth/");
-			writer.println("Search Base URL:	" + scheme + "://" + "search." + server_name + "/");
-			writer.println("Upload Base URL: 	" + scheme + "://" + "upload." + server_name + "/1/ (Not working)");
-			writer.println("--------------------------------");
-			writer.println("How to use with Twidere:");
-			writer.println("Enable \"Ignore SSL Error\", then set above URLs (It\'s better to use HTTPS.)");
-			writer.println("--------------------------------");
+			handleWelcomePage(req, resp);
 		}
+	}
+
+	private void handleWelcomePage(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		final String server_name = req.getServerName();
+		resp.setContentType("text/plain");
+		final String scheme = req.getScheme();
+		final PrintWriter writer = resp.getWriter();
+		writer.println("JTAPI " + VERSION_NAME + " is running!");
+		writer.println("--------------------------------");
+		writer.println("Rest Base URL:		" + scheme + "://" + "api." + server_name + "/1/");
+		writer.println("Oauth Base URL: 	" + scheme + "://" + "api." + server_name + "/oauth/");
+		writer.println("Search Base URL:	" + scheme + "://" + "search." + server_name + "/");
+		writer.println("Upload Base URL: 	" + scheme + "://" + "upload." + server_name + "/1/");
+		writer.println("--------------------------------");
+		writer.println("How to use with Twidere:");
+		writer.println("Enable \"Ignore SSL Error\", then set above URLs (It\'s better to use HTTPS.)");
+		writer.println("--------------------------------");
+	}
+
+	private byte[] modifyAuthorizePage(HttpServletRequest req, byte[] content) throws UnsupportedEncodingException {
+		final String server_name = req.getServerName();
+		final String request_uri = req.getRequestURI();
+		final String content_string = new String(content, "UTF-8");
+		final String replaced_content_string = content_string.replace("https://api.twitter.com/oauth/authorize",
+				"https://" + server_name + request_uri);
+		return replaced_content_string.getBytes("UTF-8");
 	}
 }
